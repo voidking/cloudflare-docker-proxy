1fec2bd448cbf136af1172dbbe1f6a360dfcf7a6
好好学习的郝 <voidking@qq.com>
2024-09-13 19:15:10 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.voidking.com": dockerHub,
+  "quay.voidking.com": "https://quay.io",
+  "gcr.voidking.com": "https://gcr.io",
+  "k8s-gcr.voidking.com": "https://k8s.gcr.io",
+  "k8s.voidking.com": "https://registry.k8s.io",
+  "ghcr.voidking.com": "https://ghcr.io",
+  "cloudsmith.voidking.com": "https://docker.cloudsmith.io",
+  "ecr.voidking.com": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.voidking.com": dockerHub,
 };
 
 function routeByHosts(host) {
